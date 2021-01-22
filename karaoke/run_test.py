2019481aa5dbb802dc7edf90b9bb0df4e1317b29
Andrew Hunter <andrewhunter1612@gmail.com>
2021-01-22 21:57:15 +0000

    Finished

--- karaoke/run_test.py
+++ karaoke/run_test.py
@@ -3,6 +3,7 @@ import unittest
 from tests.test_guests import TestGuests 
 from tests.test_rooms import TestRooms 
 from tests.test_songs import TestSongs 
+from tests.test_pub import TestPub
 
 if __name__ == "__main__":
     unittest.main()
