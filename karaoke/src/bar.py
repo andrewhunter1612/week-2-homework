c462892888ac1e31a4e0c004efbaaa8d622d3e32
Andrew Hunter <andrewhunter1612@gmail.com>
2021-01-24 14:09:32 +0000

    properly finished now...

--- karaoke/src/bar.py
+++ karaoke/src/bar.py
@@ -2,14 +2,13 @@ class Bar():
     def __init__(self, rooms):
         self.rooms = rooms
         self.cash = 0
-        self.drink_list = []
-        # self.drink_list_dict = {}
+        self.drink_list_dict = {}
 
     def add_money_to_till(self, amount):
         self.cash += amount
 
     def sell_drink(self, drink, number_sold = 1):
         self.add_money_to_till(number_sold*drink.price)
-        drink.reduce_number_available(number_sold)
-        # self.drink_list_dict[drink] = drink.number_available - number_sold
+        # drink.reduce_number_available(number_sold)
+        self.drink_list_dict[drink] = drink.number_available - number_sold
     
